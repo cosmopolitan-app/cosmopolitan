82895ca187e2e2e8afc78571d7ba9c5464f58c83
Lakshamana <guitrompa1@gmail.com>
2019-10-20 23:54:16 -0300

    added landing page

--- resources/pages/index.vue
+++ resources/pages/index.vue
@@ -1,75 +1,66 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a href="https://vuetifyjs.com" target="_blank"> documentation </a>.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat">
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a href="https://nuxtjs.org/" target="_blank">
-            Nuxt Documentation
-          </a>
-          <br />
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank">
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire">
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <v-container>
+    <v-row justify="center">
+      <v-col md="3">
+        <v-text-field
+          v-model="eventSearch"
+          placeholder="Search event..."
+        ></v-text-field>
+      </v-col>
+      <v-col md="3">
+        <v-text-field
+          v-model="talkSearch"
+          placeholder="Sarch talk..."
+        ></v-text-field>
+      </v-col>
+      <v-col md="3">
+        <v-text-field
+          v-model="dateFilter"
+          placeholder="Filter date..."
+          :value="formatDate"
+          @focus="visibleDatePicker = true"
+        ></v-text-field>
+        <template v-if="visibleDatePicker">
+          <v-date-picker no-title @input="onSelectedDate"> </v-date-picker>
+        </template>
+      </v-col>
+      <v-col md="2">
+        <v-btn class="red" tile>Search <v-icon>mdi-magnify</v-icon></v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  name: 'Index',
+  data() {
+    return {
+      eventSearch: '',
+      talkSearch: '',
+      dateFilter: '',
+      visibleDatePicker: false
+    }
+  },
+
+  computed: {},
+
+  methods: {
+    onSelectedDate(d) {
+      this.visibleDatePicker = false
+      this.dateFilter = this.formatDate(d)
+    },
+
+    formatDate(date) {
+      const [y, m, d] = date.split('-')
+      return `${m}/${d}/${y}`
+    }
   }
 }
 </script>
+
+<style scoped>
+.b {
+  border: 1px solid red;
+}
+</style>
