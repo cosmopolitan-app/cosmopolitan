c7cd9e28817919070f2aaf502680bdbe38cec312
urielfcampos <urielfcampos95@gmail.com>
2019-10-20 22:49:52 -0300

    create login page

---
+++ resources/layouts/login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Navbar :show-navbar-links="false" />
+    <nuxt />
+    <v-footer :absolute="true" color="#FF0000" height="100" dark>
+      <v-spacer></v-spacer>
+      <v-col class="text-center" cols="12">
+        &copy;Kill meetup
+      </v-col>
+    </v-footer>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/navbar'
+export default {
+  components: { Navbar }
+}
+</script>
+
+<style>
+html {
+  background-color: #8f8f8f;
+}
+.text-center {
+  text-align: center !important;
+}
+</style>
