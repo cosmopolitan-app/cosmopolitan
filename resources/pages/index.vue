070b3066ea98b7fc9601b4777fb3a01dc76a928a
Lakshamana <guitrompa1@gmail.com>
2019-10-27 17:15:21 -0300

    add axios request

--- resources/pages/index.vue
+++ resources/pages/index.vue
@@ -1,112 +1,116 @@
 <template>
   <v-container>
-    <v-row justify="center">
-      <v-col md="3">
-        <v-text-field
-          v-model="eventSearch"
-          placeholder="Search event..."
-        ></v-text-field>
-      </v-col>
-      <v-col md="3">
-        <v-text-field
-          v-model="talkSearch"
-          placeholder="Sarch talk..."
-        ></v-text-field>
-      </v-col>
-      <v-col md="3">
-        <v-text-field
-          v-model="dateFilter"
-          placeholder="Filter date..."
-          :value="formatDate"
-          @focus="visibleDatePicker = true"
+    <form @submit.prevent="onSubmit">
+      <v-row justify="center">
+        <v-col md="3">
+          <v-text-field
+            v-model="eventSearch"
+            placeholder="Search event..."
+          ></v-text-field>
+        </v-col>
+        <v-col md="3">
+          <v-text-field
+            v-model="talkSearch"
+            placeholder="Sarch talk..."
+          ></v-text-field>
+        </v-col>
+        <v-col md="3">
+          <v-text-field
+            v-model="dateFilter"
+            placeholder="Filter date..."
+            :value="formatDate"
+            @focus="visibleDatePicker = true"
+          >
+          </v-text-field>
+          <template v-if="visibleDatePicker">
+            <v-date-picker no-title @input="onSelectedDate"> </v-date-picker>
+          </template>
+        </v-col>
+        <v-col md="2">
+          <v-btn class="red" tile type="submit">
+            Search <v-icon>mdi-magnify</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+      <br />
+      <v-row justify="center">
+        <v-col class="col-sm-12 col-md-6">
+          <h1>Title</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ad
+            libero distinctio eaque ducimus nulla ex consequatur alias vitae,
+            ratione earum dolores saepe itaque temporibus deserunt autem
+            provident id sed?
+          </p>
+        </v-col>
+        <v-col class="col-sm-12 col-md-6">
+          <v-img
+            src="http://lorempixel.com/output/nature-q-c-640-480-1.jpg"
+          ></v-img>
+        </v-col>
+      </v-row>
+      <br />
+      <v-row>
+        <div class="mb-3">
+          <h1>Talkers</h1>
+        </div>
+        <v-slide-group
+          multiple
+          show-arrows
+          class="my-2"
+          prev-icon="mdi-chevron-left-circle"
+          next-icon=" mdi-chevron-right-circle"
         >
-        </v-text-field>
-        <template v-if="visibleDatePicker">
-          <v-date-picker no-title @input="onSelectedDate"> </v-date-picker>
-        </template>
-      </v-col>
-      <v-col md="2">
-        <v-btn class="red" tile>Search <v-icon>mdi-magnify</v-icon></v-btn>
-      </v-col>
-    </v-row>
-    <br />
-    <v-row justify="center">
-      <v-col class="col-sm-12 col-md-6">
-        <h1>Title</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ad
-          libero distinctio eaque ducimus nulla ex consequatur alias vitae,
-          ratione earum dolores saepe itaque temporibus deserunt autem provident
-          id sed?
-        </p>
-      </v-col>
-      <v-col class="col-sm-12 col-md-6">
-        <v-img
-          src="http://lorempixel.com/output/nature-q-c-640-480-1.jpg"
-        ></v-img>
-      </v-col>
-    </v-row>
-    <br />
-    <v-row>
-      <div class="mb-3">
-        <h1>Talkers</h1>
-      </div>
-      <v-slide-group
-        multiple
-        show-arrows
-        class="my-2"
-        prev-icon="mdi-chevron-left-circle"
-        next-icon=" mdi-chevron-right-circle"
-      >
-        <v-slide-item v-for="i in 10" :key="i">
-          <v-card class="mx-2" max-width="200">
-            <v-img
-              src="http://lorempixel.com/output/abstract-q-c-100-100-8.jpg"
-            ></v-img>
-            <v-card-title>Headline</v-card-title>
-            <v-card-text>Lorem ipsum dolor sit amet</v-card-text>
-          </v-card>
-        </v-slide-item>
-      </v-slide-group>
-    </v-row>
-    <br />
-    <v-row>
-      <div class="mb-3">
-        <h1>Talks</h1>
-      </div>
-      <v-slide-group
-        multiple
-        show-arrows
-        class="my-2"
-        prev-icon="mdi-chevron-left-circle"
-        next-icon=" mdi-chevron-right-circle"
-      >
-        <v-slide-item v-for="(e, i) in events" :key="i">
-          <v-card class="mx-2" max-width="200">
-            <v-img
-              src="http://lorempixel.com/output/abstract-q-c-100-100-8.jpg"
-            ></v-img>
-            <v-card-title>{{ e.title }}</v-card-title>
-            <v-card-subtitle class="text-truncate">
-              {{ e.subtitle }}
-            </v-card-subtitle>
-            <v-card-text class="text-truncate">
-              {{ e.text }}
-            </v-card-text>
-            <div class="is-scrollable">
-              <div style="display:inline-flex">
-                <v-chip v-for="j in 3" :key="j" class="ma-1">Category</v-chip>
+          <v-slide-item v-for="i in 10" :key="i">
+            <v-card class="mx-2" max-width="200">
+              <v-img
+                src="http://lorempixel.com/output/abstract-q-c-100-100-8.jpg"
+              ></v-img>
+              <v-card-title>Headline</v-card-title>
+              <v-card-text>Lorem ipsum dolor sit amet</v-card-text>
+            </v-card>
+          </v-slide-item>
+        </v-slide-group>
+      </v-row>
+      <br />
+      <v-row>
+        <div class="mb-3">
+          <h1>Talks</h1>
+        </div>
+        <v-slide-group
+          multiple
+          show-arrows
+          class="my-2"
+          prev-icon="mdi-chevron-left-circle"
+          next-icon=" mdi-chevron-right-circle"
+        >
+          <v-slide-item v-for="(e, i) in events" :key="i">
+            <v-card class="mx-2" max-width="200">
+              <v-img
+                src="http://lorempixel.com/output/abstract-q-c-100-100-8.jpg"
+              ></v-img>
+              <v-card-title>{{ e.title }}</v-card-title>
+              <!-- <v-card-subtitle class="text-truncate">
+                {{ e.description }}
+              </v-card-subtitle> -->
+              <v-card-text class="text-truncate">
+                {{ e.text }}
+              </v-card-text>
+              <div class="is-scrollable">
+                <div style="display:inline-flex">
+                  <v-chip v-for="j in 3" :key="j" class="ma-1">Category</v-chip>
+                </div>
               </div>
-            </div>
-            <div style="float: right">
-              <v-btn small class="my-2 mr-2 purple lighten-1">
-                Information
-              </v-btn>
-            </div>
-          </v-card>
-        </v-slide-item>
-      </v-slide-group>
-    </v-row>
+              <div style="float: right">
+                <v-btn small class="my-2 mr-2 purple lighten-1">
+                  Information
+                </v-btn>
+              </div>
+            </v-card>
+          </v-slide-item>
+        </v-slide-group>
+      </v-row>
+    </form>
   </v-container>
 </template>
 
@@ -119,58 +123,7 @@ export default {
       talkSearch: '',
       dateFilter: '',
       visibleDatePicker: false,
-      events: [
-        {
-          title: 'Headline',
-          subtitle: 'An Event',
-          text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
-        },
-        {
-          title: 'Headline',
-          subtitle: 'An Event',
-          text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
-        },
-        {
-          title: 'Headline',
-          subtitle: 'An Event',
-          text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
-        },
-        {
-          title: 'Headline',
-          subtitle: 'An Event',
-          text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
-        },
-        {
-          title: 'Headline',
-          subtitle: 'An Event',
-          text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
-        },
-        {
-          title: 'Headline',
-          subtitle: 'An Event',
-          text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
-        },
-        {
-          title: 'Headline',
-          subtitle: 'An Event',
-          text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
-        },
-        {
-          title: 'Headline',
-          subtitle: 'An Event',
-          text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
-        },
-        {
-          title: 'Headline',
-          subtitle: 'An Event',
-          text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
-        },
-        {
-          title: 'Headline',
-          subtitle: 'An Event',
-          text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
-        }
-      ]
+      events: []
     }
   },
 
@@ -183,6 +136,12 @@ export default {
     formatDate(date) {
       const [y, m, d] = date.split('-')
       return `${m}/${d}/${y}`
+    },
+
+    onSubmit() {
+      this.$axios
+        .get('/api/events')
+        .then(({ data }) => (this.events = data.data))
     }
   }
 }
